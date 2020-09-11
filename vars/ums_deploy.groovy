8dab029e30e9d815e93df6a732437c889ec821a0
dm1tryG <galkin.dmitry.485@gmail.com>
2020-09-11 22:32:38 +0300

    comma

--- vars/ums_deploy.groovy
+++ vars/ums_deploy.groovy
@@ -3,7 +3,7 @@ def call(String type, String main_branch){
     println(main_branch)
     node('UMS') {
         dir("${type}/universe") {
-            checkOutBranchOrScm(main_branch, 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', , false, false, true, 'radeonprorender-gitlab', false)
+            checkOutBranchOrScm(main_branch, 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', false, false, true, 'radeonprorender-gitlab', false)
         }
     }
 }

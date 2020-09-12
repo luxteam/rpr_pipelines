863d6ab3dc48775a00e3d8b276c21eba2c06f0ad
dm1tryG <galkin.dmitry.485@gmail.com>
2020-09-12 12:24:52 +0300

    stages

--- vars/ums_deploy.groovy
+++ vars/ums_deploy.groovy
@@ -1,11 +1,15 @@
 def call(String type, String main_branch){
     node('UMS') {
-        stage('build') {
-                dir("${type}/universe") {
-                    checkOutBranchOrScm(main_branch, 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', false, false, true, 'radeonprorender-gitlab', false)
-                    sh "docker-compose -f docker-compose.${type}.yml build"
-                    sh "docker-compose -f docker-compose.${type}.yml up -d"
-                }
+        dir("${type}/universe") {
+            stage('pull') {
+                checkOutBranchOrScm(main_branch, 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', false, false, true, 'radeonprorender-gitlab', false)
+            }
+            stage('build') {
+                sh "docker-compose -f docker-compose.${type}.yml build"
+            }
+            stage('up') {
+                sh "docker-compose -f docker-compose.${type}.yml up -d"
+            }
         }
     }
 }

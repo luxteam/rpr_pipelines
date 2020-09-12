80f507657d8f070a91d2bdba83a5942459b7f5a6
dm1tryG <galkin.dmitry.485@gmail.com>
2020-09-12 13:14:09 +0300

    add remove

--- vars/ums_deploy.groovy
+++ vars/ums_deploy.groovy
@@ -2,7 +2,15 @@ def call(String type, String main_branch){
     node('UMS') {
         dir("${type}/universe") {
             stage('preBuild') {
-                sh "sudo docker-compose -f docker-compose.${type}.yml stop"
+                try {
+                    sh "sudo docker-compose -f docker-compose.${type}.yml stop"
+                    sh "sudo docker-compose -f docker-compose.${type}.yml rm --force"
+                  } catch(Exception ex) {
+                    println("Catching the exception");
+                  }
+                dir("../") {
+                    sh "sudo rm -rf universe/"        
+                }
                 checkOutBranchOrScm(main_branch, 'https://gitlab.cts.luxoft.com/dm1tryG/universe.git', false, false, true, 'radeonprorender-gitlab', false)
             }
             stage('Build') {

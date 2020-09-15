9ea85edf3d3528981a65e306bfea89cd3616e669
dm1tryG <galkin.dmitry.485@gmail.com>
2020-09-15 19:05:44 +0300

    copy db

--- vars/ums_deploy.groovy
+++ vars/ums_deploy.groovy
@@ -1,4 +1,28 @@
 def call(String type, String main_branch){
+    String user = "admin"
+    String frontendIp = "172.30.23.112"
+    String productionContainerName = "universe_mongo_db_1"
+
+    if (type == 'devrc') {
+        node('Ubuntu18 && RBSBuilder') {
+            try {
+                println("[INFO] Try to dump production database")
+                sshagent(credentials : ['FrontendMachineCredentials']) {
+                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'FrontendMachineCredentials', usernameVariable: 'USER', passwordVariable: 'PASSWORD']]) {
+                        sh """
+                            ssh ${user}@${frontendIp} sudo docker exec -it ${productionContainerName} mongodump --archive=dump_devrc
+                            ssh ${user}@${frontendIp} sudo docker cp ${productionContainerName}:dump_devrc .
+                            ssh ${user}@${frontendIp} sudo docker exec -it ${productionContainerName} rm dump_devrc
+                        """
+                    }
+                }
+            } catch (Exception e) {
+                println("[INFO] can't dump")
+            }
+        }
+    }
+    
+    
     node('UMS') {
         dir("${type}/universe/${type}") {
             stage('preBuild') {
@@ -21,6 +45,39 @@ def call(String type, String main_branch){
             }
             stage('Up') {
                 sh "sudo docker-compose up -d"
+            }
+            if (type == 'devrc') {
+                stage('Restore database') {
+                    try {
+                        sh """
+                            sudo scp ${user}@${frontendIp}:dump_devrc /home/radmin/
+                            sudo docker cp dump_devrc ${productionContainerName}:dump_devrc
+                            sudo docker exec -it ${productionContainerName} mongorestore --archive=dump_devrc
+                            sudo docker exec -it ${productionContainerName} rm dump_devrc
+                        """
+                    } catch (Exception e) {
+                        println('not restored')
+                    }
+                    
+
+                }
+            }
+        }
+    }
+
+    if (type == 'devrc') {
+        node('Ubuntu18 && RBSBuilder') {
+            try {
+                println("[INFO] Try to drop dump")
+                sshagent(credentials : ['FrontendMachineCredentials']) {
+                    withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'FrontendMachineCredentials', usernameVariable: 'USER', passwordVariable: 'PASSWORD']]) {
+                        sh """
+                            ssh ${user}@${frontendIp} sudo docker exec -it ${productionContainerName} rm dump_devrc
+                        """
+                    }
+                }
+            } catch (Exception e) {
+                println("[INFO] can't drop dump")
             }
         }
     }

8cff9fda1c92e3c1983a53787ff66b3705afdfca
dm1tryG <galkin.dmitry.485@gmail.com>
2020-10-05 15:56:43 +0300

    drop path

--- vars/ums_client_deploy_testing.groovy
+++ vars/ums_client_deploy_testing.groovy
@@ -4,28 +4,28 @@ def call(
 ) {
     String folder = 'universe-client'
     String repoName = 'https://gitlab.cts.luxoft.com/dm1tryG/universe-client.git'
-    String compose = "docker-compose${type}.yml"
+
     node('UMS') {
         // stop container
-        dir("${type}/${folder}") {
+        dir("${type}/${folder}/deploy/${type}") {
             try {
-                sh "sudo docker-compose -f ${compose} stop";
+                sh "sudo docker-compose stop";
+                sh "sudo docker-compose rm --force"
             } catch(Exception ex) {
                 println("Catching the exception");
             }
 
-            dir("../") {
+            dir("../../../") {
                 sh "sudo rm -rf ${folder}/"        
             }
 
-            
             checkOutBranchOrScm(branch, repoName, false, false, true, 'radeonprorender-gitlab', false)
 
             stage('Build') {
-                sh "sudo docker-compose -f ${compose} build"
+                sh "sudo docker-compose build"
             }
             stage('Up') {
-                sh "sudo docker-compose -f ${compose} up -d"
+                sh "sudo docker-compose up -d"
             }
         }
     }

216723ea2f0c54976714c0cf0eae3dd216c3c148
dm1tryG <galkin.dmitry.485@gmail.com>
2020-10-05 13:59:23 +0300

    ums front

---
+++ vars/ums_client_deploy_testing.groovy
@@ -0,0 +1,32 @@
+def call(
+    String type,
+    String branch
+) {
+    String folder = 'universe-client'
+    String repoName = 'https://gitlab.cts.luxoft.com/dm1tryG/universe-client.git'
+    node('UMS') {
+        // stop container
+        dir("${type}/${folder}") {
+            try {
+                sh "sudo docker-compose -f docker-compose.${type}.yml stop";
+            } catch(Exception ex) {
+                println("Catching the exception");
+            }
+        }
+
+        dir("../") {
+            sh "sudo rm -rf ${folder}/"        
+        }
+
+        dir("../") {
+            checkOutBranchOrScm(branch, repoName, false, false, true, 'radeonprorender-gitlab', false)
+        }
+
+        stage('Build') {
+            sh "sudo docker-compose -f docker-compose${type}.yml build"
+        }
+        stage('Up') {
+            sh "sudo docker-compose -f docker-compose${type}.yml up -d"
+        }
+    }
+}

09a8e4bd22ee93506adad70cb4d4cb8677e8d94f
dm1tryG <galkin.dmitry.485@gmail.com>
2020-10-05 17:10:48 +0300

    add pipeline for api

---
+++ vars/ums_api_deploy_testing.groovy
@@ -0,0 +1,35 @@
+def call(
+    String type,
+    String branch
+) {
+    String folder = 'universe-api'
+    String repoName = 'https://gitlab.cts.luxoft.com/dm1tryG/universe-api.git'
+    String compose = "deploy/${type}/docker-compose.yml"
+    node('UMS') {
+        cleanWS("Linux")
+
+        // stop container
+        dir("${type}/${folder}") {
+            try {
+                sh "sudo docker-compose -f ${compose} stop";
+                sh "sudo docker-compose -f ${compose} rm --force"
+            } catch(Exception ex) {
+                println("Catching the exception");
+            }
+
+            dir("../../../") {
+                sh "sudo rm -rf ${folder}/"        
+            }
+
+            
+            checkOutBranchOrScm(branch, repoName, false, false, true, 'radeonprorender-gitlab', false)
+
+            stage('Build') {
+                sh "sudo docker-compose -f ${compose} build"
+            }
+            stage('Up') {
+                sh "sudo docker-compose -f ${compose} up -d"
+            }
+        }
+    }
+}

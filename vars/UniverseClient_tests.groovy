f63ace5ea24b1b7c1440e6de2fcef8e36b25f692
dm1tryG <galkin.dmitry.485@gmail.com>
2020-12-17 19:16:40 +0300

    added tests

---
+++ vars/UniverseClient_tests.groovy
@@ -0,0 +1,15 @@
+def test_create_build(parent, child) {
+    parent.createBuild('', '', false, options)
+    child.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
+}
+
+def call() {
+    node("UMS") {
+        String UMS_URL = "http://172.26.157.233:5002"
+        String IS_URL = "http://172.26.157.233:8001"
+        String product_name = "AMD Radeon™ ProRender For Blender"
+        parent_uc = new UniverseClient(this, UMS_URL, env, IS_URL, product_name)
+        child_uc = new UniverseClient(this, UniverseURLProd, env, ImageServiceURL, ProducteName, 'NorthStar ', parent_uc)
+        test_create_build(parent_uc, child_uc)
+    }
+}

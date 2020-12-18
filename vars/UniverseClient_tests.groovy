a71ffe809e4018973a5a71df800c18bdf5ccfca7
dm1tryG <galkin.dmitry.485@gmail.com>
2020-12-18 11:09:22 +0300

    upd tests

--- vars/UniverseClient_tests.groovy
+++ vars/UniverseClient_tests.groovy
@@ -1,15 +1,17 @@
-def test_create_build(parent, child) {
-    parent.createBuild('', '', false, options)
+def test_create_build() {
+    String UMS_URL = "http://172.26.157.233:5002"
+    String IS_URL = "http://172.26.157.233:8001"
+    String product_name = "AMD%20Radeon™%20ProRender%20for%20Maya"
+    
+    parent = new UniverseClient(this, UMS_URL, env, product_name)
+    parent.createBuild('', '', false, ["projectRepo":"https://github.com"])
+    
+    child = new UniverseClient(this, UMS_URL, env, IS_URL, product_name, 'NorthStar', parent)
     child.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
 }
 
 def call() {
     node("UMS") {
-        String UMS_URL = "http://172.26.157.233:5002"
-        String IS_URL = "http://172.26.157.233:8001"
-        String product_name = "AMD Radeon™ ProRender For Blender"
-        parent_uc = new UniverseClient(this, UMS_URL, env, IS_URL, product_name)
-        child_uc = new UniverseClient(this, UMS_URL, env, IS_URL, product_name, 'NorthStar ', parent_uc)
-        test_create_build(parent_uc, child_uc)
+        test_create_build()
     }
 }

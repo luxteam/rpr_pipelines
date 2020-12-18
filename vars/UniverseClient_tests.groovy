5b599b4f0d35a3e8d75635a95d3aee7a245bfe5d
dm1tryG <galkin.dmitry.485@gmail.com>
2020-12-18 15:39:59 +0300

    fix typo

--- vars/UniverseClient_tests.groovy
+++ vars/UniverseClient_tests.groovy
@@ -3,7 +3,7 @@ def test_create_build() {
         string(credentialsId: 'testing2UniverseURL', variable: 'TEST2_UMS_URL'),
         string(credentialsId: 'imageServiceURL', variable: 'IS_URL')
     ]) {
-        String UMS_URL  = "${PROD_UMS_URL}"
+        String UMS_URL  = "${TEST2_UMS_URL}"
         String IS_URL = "${IS_URL}"
     }
     String product_name = "AMD%20Radeon™%20ProRender%20for%20Maya"

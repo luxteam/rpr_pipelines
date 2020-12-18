338d43a1e395068e3ddde13b065a97d786b85ba1
dm1tryG <galkin.dmitry.485@gmail.com>
2020-12-18 15:42:45 +0300

    move all body to with creds block

--- vars/UniverseClient_tests.groovy
+++ vars/UniverseClient_tests.groovy
@@ -5,16 +5,17 @@ def test_create_build() {
     ]) {
         String UMS_URL  = "${TEST2_UMS_URL}"
         String IS_URL = "${IS_URL}"
-    }
-    String product_name = "AMD%20Radeon™%20ProRender%20for%20Maya"
-    
-    parent = new UniverseClient(this, UMS_URL, env, product_name)
-    parent.tokenSetup()
-    parent.createBuild('', '', false, ["projectRepo":"https://github.com"])
 
-    child = new UniverseClient(this, UMS_URL, env, IS_URL, product_name, 'NorthStar', parent)
-    child.tokenSetup()
-    child.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
+        String product_name = "AMD%20Radeon™%20ProRender%20for%20Maya"
+        
+        parent = new UniverseClient(this, UMS_URL, env, product_name)
+        parent.tokenSetup()
+        parent.createBuild('', '', false, ["projectRepo":"https://github.com"])
+
+        child = new UniverseClient(this, UMS_URL, env, IS_URL, product_name, 'NorthStar', parent)
+        child.tokenSetup()
+        child.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
+    }
 }
 
 def call() {

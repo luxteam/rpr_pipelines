cb8196b28f140db3eb47abf1185d92ab5a3ea7a0
dm1tryG <galkin.dmitry.485@gmail.com>
2021-01-15 11:24:19 +0300

    skip url map

--- vars/UniverseClient_tests.groovy
+++ vars/UniverseClient_tests.groovy
@@ -3,19 +3,20 @@ def test_create_build() {
         string(credentialsId: 'testing2UniverseURL', variable: 'TEST2_UMS_URL'),
         string(credentialsId: 'imageServiceURL', variable: 'IS_URL')
     ]) {
-        String UMS_URL  = "${TEST2_UMS_URL}"
-        String IS_URL = "${IS_URL}"
+        
+        String umsURL  = "${TEST2_UMS_URL}"
+        String isURL = "${IS_URL}"
+        String productName = "AMD%20Radeon™%20ProRender%20for%20Maya"
 
-        String product_name = "AMD%20Radeon™%20ProRender%20for%20Maya"
-
-        parent = new UniverseClient(this, UMS_URL, env, product_name)
+        parent = new UniverseClient(this, umsURL, env, productName)
         parent.tokenSetup()
         parent.createBuild('', '', false, ["projectRepo":"https://github.com"])
 
-        child = new UniverseClient(this, UMS_URL, env, IS_URL, product_name, 'NorthStar', parent)
+        child = new UniverseClient(this, umsURL, env, isURL, productName, 'NorthStar', parent)
         child.tokenSetup()
         child.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
         child.changeStatus("SUCCESS")
+        
         parent.changeStatus("SUCCESS")
     }
 }

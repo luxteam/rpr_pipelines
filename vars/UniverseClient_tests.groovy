c9dd6d9e776f1754b7bfd5939581edbdc905b8b8
dm1tryG <galkin.dmitry.485@gmail.com>
2021-01-19 16:44:27 +0300

    added 2 child

--- vars/UniverseClient_tests.groovy
+++ vars/UniverseClient_tests.groovy
@@ -3,20 +3,28 @@ def test_create_build() {
         string(credentialsId: 'testing2UniverseURL', variable: 'TEST2_UMS_URL'),
         string(credentialsId: 'imageServiceURL', variable: 'IS_URL')
     ]) {
-        
+        println('Wrapper testing')
+
+        // configuration
         String umsURL  = "${TEST2_UMS_URL}"
         String isURL = "${IS_URL}"
         String productName = "AMD%20Radeon™%20ProRender%20for%20Maya"
 
+        
         parent = new UniverseClient(this, umsURL, env, productName)
         parent.tokenSetup()
         parent.createBuild('', '', false, ["projectRepo":"https://github.com"])
 
-        child = new UniverseClient(this, umsURL, env, isURL, productName, 'NorthStar', parent)
-        child.tokenSetup()
-        child.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
-        child.changeStatus("SUCCESS")
+        child1 = new UniverseClient(this, umsURL, env, isURL, productName, 'Northstar', parent)
+        child1.tokenSetup()
+        child1.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
+        child1.changeStatus("SUCCESS")
         
+        child2 = new UniverseClient(this, umsURL, env, isURL, productName, 'Tahoe', parent)
+        child2.tokenSetup()
+        child2.createBuild(["Windows-AMD", "Windows-OSX"], ["Smoke", "Sanity"], false)
+        child2.changeStatus("SUCCESS")
+
         parent.changeStatus("SUCCESS")
     }
 }

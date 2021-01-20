a9083783a69c33c14c5d8de7973976ffae2aafcd
dm1tryG <galkin.dmitry.485@gmail.com>
2021-01-20 19:20:38 +0300

    params to execute

--- vars/UniverseClient_tests.groovy
+++ vars/UniverseClient_tests.groovy
@@ -23,8 +23,15 @@ def test_create_build() {
         cleanWS("Ubuntu")
         checkOutBranchOrScm('ums_tests', 'https://github.com/luxteam/jobs_launcher.git')
 
+        def UMS_URL='https://umsapi2.cistest.luxoft.com'
+        def UMS_JOB_ID='5f881e5a8e8872f31dc413e3'
+        def UMS_ENV_LABEL='Windows-AMD'
+        def UMS_LOGIN='dm1tryG'
+        def UMS_PASSWORD='root'
+        def UMS_BUILD_ID='6007fd00cf43c198e4d3e298'
+
         sh """
-            sudo sh run_ums_tests.sh >> ../tests.log 2>&1
+            sudo sh run_ums_tests.sh ${UMS_URL} ${UMS_JOB_ID} ${UMS_ENV_LABEL} ${UMS_LOGIN} ${UMS_PASSWORD} ${UMS_BUILD_ID}>> ../tests.log 2>&1
         """
 
         child2 = new UniverseClient(this, umsURL, env, isURL, productName, 'Tahoe', parent)

16472993345e31899857c3525e169e8b7d77d491
Ilia Nemankov <INemankov@luxoft.com>
2021-03-15 08:56:27 +0300

    Remove uniting of tests in blender

--- vars/rpr_blender28plugin_pipeline.groovy
+++ vars/rpr_blender28plugin_pipeline.groovy
@@ -810,7 +810,7 @@ def executePreBuild(Map options)
                     }
                 }
 
-                options.tests = utils.uniteSuites(this, "jobs/weights.json", tempTests)
+                options.tests = tempTests
                 options.tests.each() {
                     def xml_timeout = utils.getTimeoutFromXML(this, "${it}", "simpleRender.py", options.ADDITIONAL_XML_TIMEOUT)
                     options.timeouts["${it}"] = (xml_timeout > 0) ? xml_timeout : options.TEST_TIMEOUT
@@ -834,7 +834,7 @@ def executePreBuild(Map options)
                 }
             } else if (options.tests) {
                 options.groupsUMS = options.tests.split(" ") as List
-                options.tests = utils.uniteSuites(this, "jobs/weights.json", options.tests.split(" ") as List)
+                options.tests = options.tests.split(" ") as List
                 options.tests.each() {
                     options.engines.each { engine ->
                         tests << "${it}-${engine}"
